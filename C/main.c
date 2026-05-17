8607dd0e2c3d70f4722c42fea32418a9c8ab43de
spiderasasen <spiderasasen@gmail.com>
2026-05-17 11:57:13 -0400

    got the string splitting in half

--- C/main.c
+++ C/main.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 
+//making the functions avalible
+int getLength(char* string);
+void inHalf(char* string);
+
 int main() {
-    printf("Hello World");
+    //main strings
+    char* string1 = "bill";
+    char* string2 = "Hello";
+
+    //printing the half of the string
+    char* halfString1 = string1;
+    char* halfString2 = string2;
+
+    printf("Printing the half of the string\n");
+    printf("Original Strings: %s, %s\n", halfString1, halfString2);
+    inHalf(halfString1);
+    inHalf(halfString2);
+    printf("New Strings: %s, %s\n", halfString1, halfString2);
 
     return 0;
+}
+
+//spliting and printing the string in half
+void inHalf(char* string) {
+    //getting the half of the string
+    int length = getLength(string);
+    int halfLength = length / 2;
+
+    //only changing the the string in the half
+    for (int i = 0; i < halfLength; i++) {
+        putchar(string[i]);
+    }
+    putchar('\n');
+}
+
+//spliting the string in half, and only printing the ending half
+
+//printing the string in reverse
+
+//getting the length of the string
+int getLength(char* string) {
+    int length = 0;
+    while (*string) {
+        length++;
+        string++;
+    }
+    return length;
 }
